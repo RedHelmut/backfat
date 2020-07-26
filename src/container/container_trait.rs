2c095d604ce5a799f75c81abb4f65c36534540c4
RedHelmut <the_red_helmut@hotmail.com>
2020-07-26 13:51:26 -0600

    Refresh

--- src/container/container_trait.rs
+++ src/container/container_trait.rs
@@ -1,16 +1,18 @@
 use crate::container::placement_info::PlacementInfo;
 use crate::container::rectangle::Border;
 use std::cell::RefCell;
+use lopdf::content::Operation;
 
 pub trait GroupTracker {}
 
 pub trait DrawInfoReq {
     fn increment_page_buffer(&mut self, page_number: usize);
     fn page_array_size(&self) -> usize;
+    fn insert_into_page(&mut self, page_num: usize, operation: Operation);
 }
 
-pub trait ContainerTrait<T: DrawInfoReq> {
-    fn on_draw(
+pub trait ContainerTrait {
+    fn on_draw<T: DrawInfoReq>(
         &mut self,
         placement_info: PlacementInfo,
         draw_info: &mut T,
