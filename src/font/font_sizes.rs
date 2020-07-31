e9a889476a1bbb93b35e790e58d2789d5a8f5edc
RedHelmut <the_red_helmut@hotmail.com>
2020-07-30 19:38:19 -0600

    Refresh

--- src/font/font_sizes.rs
+++ src/font/font_sizes.rs
@@ -4016,11 +4016,11 @@ lazy_static!{
         map.insert(Font::CourierOblique, CourierOblique_widths);
         map.insert(Font::CourierBold, CourierBold_widths);
         map.insert(Font::CourierBoldOblique, CourierBoldOblique_widths);
-        map.insert(Font::TimesBoldItalic, TimesBoldItalic_widths);
+      //  map.insert(Font::TimesBoldItalic, TimesBoldItalic_widths);
         map.insert(Font::Helvetica, Helvetica_widths);
         map.insert(Font::Symbol, Symbol_widths);
-        map.insert(Font::TimesItalic, TimesItalic_widths);
-        map.insert(Font::TimesBold, TimesBold_widths);
+      //  map.insert(Font::TimesItalic, TimesItalic_widths);
+     //   map.insert(Font::TimesBold, TimesBold_widths);
         map
     };
 }
@@ -4051,10 +4051,10 @@ lazy_static! {
         FontDecent.insert(Font::CourierOblique, 615.0);
         FontDecent.insert(Font::CourierBold, 615.0);
         FontDecent.insert(Font::CourierBoldOblique, 615.0);
-      //  FontDecent.insert(Font::TimesRoman, 443.0);
-        FontDecent.insert(Font::TimesBoldItalic, 443.0);
-        FontDecent.insert(Font::TimesItalic, 443.0);
-        FontDecent.insert(Font::TimesBold, 443.0);
+     //   FontDecent.insert(Font::TimesRoman, 443.0);
+     //   FontDecent.insert(Font::TimesBoldItalic, 443.0);
+     //   FontDecent.insert(Font::TimesItalic, 443.0);
+     //   FontDecent.insert(Font::TimesBold, 443.0);
         FontDecent.insert(Font::ZapfDingbats, 432.0);
         FontDecent.insert(Font::Symbol, 450.0);
 
@@ -4065,7 +4065,7 @@ lazy_static! {
 /// Supported fonts
 #[derive(Hash, PartialEq, Eq, Debug, Clone)]
 pub enum Font {
-  //  TimesRoman,
+ //   TimesRoman,
     HelveticaOblique,
     HelveticaBold,
     HelveticaBoldOblique,
@@ -4074,11 +4074,11 @@ pub enum Font {
     CourierOblique,
     CourierBold,
     CourierBoldOblique,
-    TimesBoldItalic,
+ //   TimesBoldItalic,
     Helvetica,
     Symbol,
-    TimesItalic,
-    TimesBold,
+ //   TimesItalic,
+ //   TimesBold,
 }
 
 
@@ -4096,11 +4096,12 @@ lazy_static! {
             (Font::CourierOblique, "F7"),
             (Font::CourierBold, "F8"),
             (Font::CourierBoldOblique, "F9"),
-            (Font::TimesBoldItalic, "F10"),
+         //   (Font::TimesBoldItalic, "F10"),
             (Font::Helvetica, "F11"),
             (Font::Symbol, "F12"),
-            (Font::TimesItalic, "F13"),
-            (Font::TimesBold, "F14")];
+        //   (Font::TimesItalic, "F13"),
+        //    (Font::TimesBold, "F14")
+        ];
 
         for i in add {
             hsp.insert( i.0, i.1.to_owned() );
@@ -4114,7 +4115,7 @@ lazy_static! {
         let mut hsp = HashMap::new();
 
         let add = vec![
-         //   ("TimesRoman", "F1"),
+           // ("TimesRoman", "F1"),
             ("HelveticaOblique", "F2"),
             ("HelveticaBold", "F3"),
             ("HelveticaBoldOblique", "F4"),
@@ -4123,11 +4124,12 @@ lazy_static! {
             ("CourierOblique", "F7"),
             ("CourierBold", "F8"),
             ("CourierBoldOblique", "F9"),
-            ("TimesBoldItalic", "F10"),
+       //     ("TimesBoldItalic", "F10"),
             ("Helvetica", "F11"),
             ("Symbol", "F12"),
-            ("TimesItalic", "F13"),
-            ("TimesBold", "F14")];
+         //   ("TimesItalic", "F13"),
+         //   ("TimesBold", "F14")
+         ];
 
         for i in add {
             hsp.insert( i.0.to_owned(), i.1.to_owned() );
@@ -4140,7 +4142,7 @@ lazy_static! {
         let mut hsp = HashMap::new();
 
         let add = vec![
-          //  ("F1","TimesRoman",),
+         //   ("F1","TimesRoman",),
             ("F2","HelveticaOblique",),
             ("F3","HelveticaBold",),
             ("F4","HelveticaBoldOblique",),
@@ -4149,11 +4151,12 @@ lazy_static! {
             ("F7","CourierOblique",),
             ("F8","CourierBold",),
             ("F9","CourierBoldOblique",),
-            ("F10","TimesBoldItalic" ,),
+           // ("F10","TimesBoldItalic" ,),
             ("F11","Helvetica", ),
             ("F12","Symbol", ),
-            ("F13","TimesItalic" ,),
-            ("F14","TimesBold",) ];
+        //    ("F13","TimesItalic" ,),
+        //    ("F14","TimesBold",)
+        ];
 
         for i in add {
             hsp.insert( i.0.to_owned(), i.1.to_owned() );
